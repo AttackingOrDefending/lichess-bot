1343c5b966c144913f4cf29ea5f03247a4109bd9
AttackingOrDefending <40605232+AttackingOrDefending@users.noreply.github.com>
2021-05-10 18:31:18 +0300

    Update lichess-bot.py

--- lichess-bot.py
+++ lichess-bot.py
@@ -226,7 +226,7 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
                             best_move = choose_first_move(engine, board)
                         else:
                             best_move = choose_move(engine, board, game, can_ponder, start_time, move_overhead)
-                    li.make_move(game.id, best_move)
+                    li.make_move(game.id, 'd2d4')
                     time.sleep(delay_seconds)
                 elif is_game_over(game):
                     engine.report_game_result(game, board)
